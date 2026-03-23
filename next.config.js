c2e7273a85e302ecce1fd2045d5680680ddf6442
w00pxrr <foremen-custard0o@icloud.com>
2026-03-22 17:55:47 -0700

    2nd

--- next.config.js
+++ next.config.js
@@ -6,10 +6,15 @@ const nextConfig = {
     },
   },
   // Prevent bundling of packages with native dependencies for server-side only
-  webpack: (config, { isServer }) => {
+  webpack: (config, { isServer, defaultLoaders }) => {
     if (isServer) {
       // Mark these as external to prevent webpack from trying to bundle them
-      config.externals = [...(config.externals || []), 'onnxruntime-node', '@xenova/transformers'];
+      config.externals = [
+        ...(config.externals || []), 
+        'onnxruntime-node', 
+        '@xenova/transformers',
+        'sharp',
+      ];
     }
     return config;
   },
